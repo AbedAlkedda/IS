e1c11c831c0d70822402d4ae0ae86e5c4eb2fa8e
abed <zagdkde@gmail.com>
2023-02-20 23:23:31 +0100

    server_a file renamed

---
+++ server_a/soap_client.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'savon'
+
+# SOAP Client
+class SOAPClient
+  def initialize(wsdl_url)
+    @client = Savon.client(wsdl: wsdl_url)
+  end
+
+  def get_titles(count)
+    response = @client.call(:titles, message: { elements: count })
+    response.body[:titles_response][:titles_result][:string]
+  end
+end
+
+client = SOAPClient.new('http://localhost:5000?wsdl')
+titles = client.get_titles(10)
+
+puts titles
