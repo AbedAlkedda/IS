299e65dba233cb72a556a15b3dac2163bce3b7b8
abed <zagdkde@gmail.com>
2023-02-22 23:49:03 +0100

    htwk done dynamicly

--- rest/app.rb
+++ rest/app.rb
@@ -9,9 +9,11 @@ set :port, 5000
 soap_client = SOAPClient.new(ENV['SOAP_CLIENT_URL'] || 'http://localhost:8000?wsdl')
 
 get '/' do
-  @movies = soap_client.movies_data 2
+  @movies       = soap_client.movies_data 2
+  @semester_num = 2
 
-  soap_client.htwk_info 2
+  soap_client.htwk_info @semester_num
+
   @htwk_infos    = soap_client.htwk_infos
   @semeter_infos = soap_client.semeter_infos
 
@@ -23,4 +25,15 @@ post '/update-list' do
   @movies = soap_client.movies_data data.to_i
 
   @movies.to_json
+end
+
+post '/update-htwk-info' do
+  data          = JSON.parse request.body.read
+  @semester_num = data.to_i
+
+  soap_client.htwk_info @semester_num
+  @htwk_infos    = soap_client.htwk_infos
+  @semeter_infos = soap_client.semeter_infos
+
+  [@htwk_infos, @semeter_infos].to_json
 end
